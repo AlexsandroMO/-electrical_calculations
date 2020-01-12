7585d00c522f27fd9d7379b0b72ca7650f9de685
Alexsandro <34524511+Sam-Corleone@users.noreply.github.com>
2020-01-12 16:40:59 -0300

    upgrate

--- calc/admin.py
+++ calc/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import IsolatioType, InstalatioType, CableCalculator
+
+
+class ListaCableCalculator(admin.ModelAdmin):
+    list_display = ('instalation','isolation','number_polos', 'corrente_ckt') 
+    
+
+admin.site.register(CableCalculator, ListaCableCalculator)
+admin.site.register(IsolatioType)
+admin.site.register(InstalatioType)
+
